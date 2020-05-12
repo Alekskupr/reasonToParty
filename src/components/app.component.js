4b434f0c5a56ecefec3d0f571fad30e05e6c7f51
Alekskupr <leshakuprish@yandex.ru>
2020-05-12 23:44:14 +0300

    step2

---
+++ src/components/app.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import s from './app.component.css';
+
+class MyComponent extends Component {
+  render() {
+    return <div className={s.intro}>TEST!!!!!!!!!!!!!!</div>;
+  }
+}
+
+export default MyComponent;
